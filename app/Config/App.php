862220b56448cb28ba1a82b67ebbb7275ab2ecf6
KAnggara <kanggara75@gmail.com>
2021-12-08 21:12:12 +0700

    fix:  on App

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://borang.kanggara.my.id/';
+    public $baseURL = 'https://borang.kanggara.my.id/';
 
     /**
      * --------------------------------------------------------------------------
