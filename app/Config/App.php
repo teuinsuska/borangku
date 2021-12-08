3fc079ddee5623e34d1067aa9671b356fb01df65
Call Vin <kanggara75@users.noreply.github.com>
2021-12-08 20:34:10 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'https://borang.kanggara.my.id/';
 
     /**
      * --------------------------------------------------------------------------
