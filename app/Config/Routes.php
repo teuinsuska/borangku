bc93788887aa0afe85ef15f06e8bcea171a5c73e
KAnggara <kanggara75@gmail.com>
2021-12-12 22:36:38 +0700

    add: Login, logout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,9 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+// Auth
+$routes->get('/login', 'Auth::index');
+$routes->post('/login', 'Auth::login');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
