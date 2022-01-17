17a57ccea2868f035d6dc2d49740268baf9df292
KAnggara <kanggara75@gmail.com>
2022-01-17 14:07:47 +0700

    fix: cek kelengkapan data (PA)

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -34,12 +34,16 @@ class Dashboard extends Controller
 
   public function user()
   {
+    // Ambil data
     $session = session();
     $nim = $session->get('nim');
     $name = strtolower($session->get('name'));
-    $cek = strlen($nim);
+    $pa = $session->get('pa');
+    // cek angkatan
     $trimangkatan = substr($nim, 1, -8);
     $angkatan = is_numeric($trimangkatan) ? '2000'  + $trimangkatan : '-';
+    // Cek Status
+    $cek = strlen($nim);
     if ($cek == 11) {
       $status = "Mahasiswa";
     } else {
@@ -48,17 +52,23 @@ class Dashboard extends Controller
     $data =
       [
         'email' => $session->get('email'),
+        'hp' => $session->get('hp'),
         'title' => ucwords($name),
         'name' => ucwords($name),
-        'hp' => $session->get('hp'),
         'angkatan' => $angkatan,
         'status' => $status,
         'nim' => $nim,
+        'pa' => $pa,
       ];
     echo view('template/head', $data);
     echo view('template/sidebar', $data);
     echo view('template/topbar', $data);
-    echo view('user/user', $data);
+    // Cek Kelengkapan data
+    if (isset($pa)) {
+      echo view('user/user', $data);
+    } else {
+      echo view('user/profile', $data);
+    }
     echo view('template/foot', $data);
   }
 }
